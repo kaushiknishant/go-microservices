818a785b6a72e4cd61b08b242263a2e025d2f9be
FIRST_NAME LAST_NAME <nishant@navigus.in>
2021-07-22 19:05:45 +0530

    code formatting

--- product-api/main.go
+++ product-api/main.go
@@ -3,30 +3,29 @@ package main
 import (
 	"net/http"
 	//"log"
-	"io/ioutil"
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
-http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
-	// log.Println("Hello world")
-	response, err:= ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(rw, "Bad Request", http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(rw,"Hello %s",response)
-})
+	http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request) {
+		// log.Println("Hello world")
+		response, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Hello %s", response)
+	})
 
-http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request){
-	res, err:= ioutil.ReadAll(r.Body)
-	if err != nil {
-		http.Error(rw, "Bad Request", http.StatusBadRequest)
-		return
-	}
-	fmt.Fprintf(rw,"Good Bye %s",res)
-})
+	http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request) {
+		res, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(rw, "Bad Request", http.StatusBadRequest)
+			return
+		}
+		fmt.Fprintf(rw, "Good Bye %s", res)
+	})
 
 	http.ListenAndServe(":9090", nil)
 }
-
