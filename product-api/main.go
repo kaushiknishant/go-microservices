82bae146375ba54cb77b3af88c8976274b92e37d
FIRST_NAME LAST_NAME <nishant@navigus.in>
2021-07-22 18:09:36 +0530

    basic server code with two routes

--- product-api/main.go
+++ product-api/main.go
@@ -2,12 +2,29 @@ package main
 
 import (
 	"net/http"
-	"log"
+	//"log"
+	"io/ioutil"
+	"fmt"
 )
 
 func main() {
-http.HandleFunc("/", func(http.ResponseWriter, *http.Request){
-	log.Println("Hello world")
+http.HandleFunc("/", func(rw http.ResponseWriter, r *http.Request){
+	// log.Println("Hello world")
+	response, err:= ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(rw,"Hello %s",response)
+})
+
+http.HandleFunc("/goodbye", func(rw http.ResponseWriter, r *http.Request){
+	res, err:= ioutil.ReadAll(r.Body)
+	if err != nil {
+		http.Error(rw, "Bad Request", http.StatusBadRequest)
+		return
+	}
+	fmt.Fprintf(rw,"Good Bye %s",res)
 })
 
 	http.ListenAndServe(":9090", nil)
