703178a7a755b3de4bf40246fd3d469121e52c4d
FIRST_NAME LAST_NAME <nishant@navigus.in>
2021-07-24 14:50:16 +0530

    adding timeout and graceful shutdown of app

--- product-api/main.go
+++ product-api/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"log"
 	"net/http"
 	"os"
+	"os/signal"
+	"time"
 
 	"github.com/kaushiknishant/go-microservices/product-api/handlers"
 )
@@ -17,5 +20,31 @@ func main() {
 	serveMux.Handle("/", helloHandler)
 	serveMux.Handle("/goodbye", goodByeHandler)
 
-	http.ListenAndServe(":9090", serveMux)
+	server := &http.Server{
+		Addr:         ":9090",
+		Handler:      serveMux,
+		IdleTimeout:  120 * time.Second,
+		ReadTimeout:  1 * time.Second,
+		WriteTimeout: 1 * time.Second,
+	}
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			logs.Fatal(err)
+		}
+	}()
+
+	sigChan := make(chan os.Signal)
+	signal.Notify(sigChan, os.Interrupt)
+	signal.Notify(sigChan, os.Kill)
+
+	sig := <-sigChan
+	logs.Println("Received terminate, graceful shutdown", sig)
+
+	duration := time.Now().Add(30 * time.Second)
+	timeOutContext, cancel := context.WithDeadline(context.Background(), duration)
+
+	defer cancel()
+
+	server.Shutdown(timeOutContext)
 }
